a8ec90b2de9a55206b23f8ab7e443edfb65ba242
T. Josh PItts <50296158+Joshpitts16@users.noreply.github.com>
2023-04-26 20:55:04 -0400

    Product Controller and VM changes

---
+++ FinalProject_AdvancedWeb/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FinalProject_AdvancedWeb.Models.Entities;
+using FinalProject_AdvancedWeb.Models.ViewModels;
+namespace FinalProject_AdvancedWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IShelfRepository _shelfRepo;
+
+        public ProductController(IShelfRepository shelfRepo)
+        {
+            _shelfRepo = shelfRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> CreateAsync([Bind(Prefix = "id")] int shelfId)
+        {
+            var book = await _shelfRepo.ReadAsync(shelfId);
+            if (book == null)
+            {
+                RedirectToAction("Index", "shelf");
+            }
+            ViewData["shelf"] = Shelf;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int shelfId, CreateProductVM productVM)
+        {
+            if (productVM != null)
+            {
+                var model = productVM.GetProductInstance();
+                await _shelfRepo.CreateAuthorAsync(shelfId, model);
+                return RedirectToAction("Details", "Shelf", new { id = shelfId });
+            }
+            var shelf = _shelfRepo.ReadAsync(shelfId);
+            ViewData["Shelf"] = shelf;
+            return View(productVM);
+        }
+        public async Task<IActionResult> Edit([Bind(Prefix = "id")] int shelfId, int productId)
+        {
+            var shelf = await _shelfRepo.ReadAsync(shelfId);
+            if (shelf == null)
+            {
+                return RedirectToAction("Index", "Shelf");
+            }
+            var product = Shelf.Products.FirstOrDefault(a => a.Id == productId);
+            if (product == null)
+            {
+                return RedirectToAction("Details", "Shelf");
+            }
+            var model = new EditProductVM()
+            {
+                shelf = shelf,
+                id = product.Id,
+                ProductName = product.ProductName,
+                Quantity = product.Quantity
+            };
+            return View(model);
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int shelfId, EditProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = productVM.GetProductInstance();
+                await _shelfRepo.UpdateAuthorAsync(shelfId, product);
+                return RedirectToAction("Details", "shelf", new { id = shelfId });
+            }
+            productVM.shelf = await _shelfRepo.ReadAsync(shelfId);
+            return View(productVM);
+        }
+        public async Task<IActionResult> Delete([Bind(Prefix = "id")] int shelfId, int productId)
+        {
+            var shelf = await _shelfRepo.ReadAsync(shelfId);
+            if (shelf == null)
+            {
+                return RedirectToAction("Index", "Shelf");
+            }
+            var product = shelf.Authors.FirstOrDefault(a => a.Id == productId);
+            if (product == null)
+            {
+                return RedirectToAction("Details", "Shelf", new { id = shelfId });
+            }
+            var model = new DeleteProductVM()
+            {
+                shelf = shelf,
+                id = product.Id,
+                ProductName = product.ProductName,
+                Quantity = product.Quantity
+            };
+            return View(model);
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int shelfId, int productId)
+        {
+            await _shelfRepo.DeleteAuthorAsync(shelfId, productId);
+            return RedirectToAction("Details", "Shelf", new { id = shelfId });
+        }
+    }
+}
---
+++ FinalProject_AdvancedWeb/Models/ViewModels/DeleteProductVM.cs
@@ -0,0 +1,11 @@
+﻿using FinalProject_AdvancedWeb.Models.Entities;
+namespace FinalProject_AdvancedWeb.Models.ViewModels
+{
+    public class DeleteProductVM
+    {
+        public Shelf? shelf { get; set; }
+        public int id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
